8d4f7931a4d11f119ca3715f23c10c95c0b7063d
Bavaria1222 <carlos.alvarez.1222@outlook.com>
2024-10-24 06:28:24 -0600

    Primer Comit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Bavaria1222/godesde0
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Bavaria1222/godesde0/variables"
+)
+
+func main(){
+	variables.MuestroEnteros()
+}
